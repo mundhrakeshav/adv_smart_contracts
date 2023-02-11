dea39449d4b0125a8d9c14a8cfbd15e8a3474690
Keshav Mundhra <mundhrakeshav11@gmail.com>
2023-02-11 23:32:06 +0530

    Ethernaut: Fallback

---
+++ test/CTFs/ethernaut/BaseTest.t.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+
+abstract contract BaseTest is Test {
+    string RPC_URL = "https://arb-goerli.g.alchemy.com/v2/3U5uAERPean0YfI-BApMmL1E3nv_GSJX";
+
+    function setUp() public virtual {
+        uint256 forkId = vm.createFork(RPC_URL, 7179371);
+        vm.selectFork(forkId);
+        hoax(address(this));
+    }
+
+    function exploitSol() internal virtual;
+    function exploitYul() internal virtual;
+
+    function testExploitSol() external virtual;
+    function testExploitYul() external virtual;
+}
