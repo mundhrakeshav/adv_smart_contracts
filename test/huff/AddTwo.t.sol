b72ca157d953500fb798df5998557a9a4930f3bd
Keshav Mundhra <mundhrakeshav11@gmail.com>
2023-02-12 15:20:21 +0530

    Add Huff

---
+++ test/huff/AddTwo.t.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+import "forge-std/Test.sol";
+import {HuffDeployer} from "../../lib/foundry-huff/src/HuffDeployer.sol";
+import {IAddTwo} from "../../src/huff/IAddTwo.sol";
+
+contract HuffTest is Test {
+    IAddTwo addTwoContract;
+
+    function setUp() public {
+        // Deploy a new instance of src/test/contracts/Number.huff
+        address addr = HuffDeployer.deploy("huff/AddTwo");
+        // To call a function on the deployed contract, create an interface and wrap the address like so
+        addTwoContract = IAddTwo(addr);
+    }
+
+    function testPayToReceiverContractWithoutCallDataZ(uint256 a, uint256 b) public {
+        vm.assume(a < 2 ** 128);
+        vm.assume(b < 2 ** 128);
+        assertEq(addTwoContract.addTwo(a, b), a + b);
+    }
+}
