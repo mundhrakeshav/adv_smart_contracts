c211e074100378039540903f3fea013a0df519d5
Keshav Mundhra <mundhrakeshav11@gmail.com>
2023-04-19 15:24:53 +0530

    Contract -> interface

--- src/CTFs/ethernaut/IGateKeeperTwo.sol
+++ src/CTFs/ethernaut/IGateKeeperTwo.sol
@@ -1,29 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract IGateKeeperTwo {
-
-  address public entrant;
-
-  modifier gateOne() {
-    require(msg.sender != tx.origin, "TXORIGIN");
-    _;
-  }
-
-  modifier gateTwo() {
-    uint x;
-    assembly { x := extcodesize(caller()) }
-    require(x == 0,"T2");
-    _;
-  }
-
-  modifier gateThree(bytes8 _gateKey) {
-    require(uint64(bytes8(keccak256(abi.encodePacked(msg.sender)))) ^ uint64(_gateKey) == type(uint64).max, "G3");
-    _;
-  }
-
-  function enter(bytes8 _gateKey) public gateOne gateTwo gateThree(_gateKey) returns (bool) {
-    entrant = tx.origin;
-    return true;
-  }
+interface IGateKeeperTwo {
+    function entrant() external returns(address);
+    function enter(bytes8 _gateKey) external;
 }
